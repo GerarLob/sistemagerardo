c0e0be4d35b05d361b54dd66f7844c522a91f2c2
Adbelss <adbel1311@gmail.com>
2025-07-28 20:40:30 -0600

    Eliminada página de bienvenida - Redirección directa al dashboard

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,4 @@
 from django.contrib.auth.views import LoginView
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 
 class CustomLoginView(LoginView):
     template_name = 'usuarios/login.html'
-
-@login_required
-def bienvenida(request):
-    return redirect('dashboard:dashboard_home')
